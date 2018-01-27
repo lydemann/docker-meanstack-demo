4e2ae7703d1003c65cc2c3aa444769a8ff8278b8
Christian <christianlydemann@gmail.com>
2018-01-27 10:25:13 +0100

    setup dockerfile

--- server/routes.js
+++ server/routes.js
@@ -2,15 +2,17 @@ const path = require('path');
 const express = require('express');
 
 exports = module.exports = function(app) {
+
+    const staticFilesPath = path.resolve('./static');
   
     app.get('/hello', (req, res) => res.send('Hello World!'))
 
-    app.use(express.static(path.resolve('../client/dist')))
+    app.use(express.static(staticFilesPath))
 
     // All other routes should redirect to the index.html
     app.route('/*')
       .get((req, res) => {
-        res.sendFile(path.resolve('./static/dist') + '/index.html');
+        res.sendFile(staticFilesPath + '/index.html');
       });
   }
   
