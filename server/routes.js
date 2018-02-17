946636f4030d476385e7edcb1141b15bbc60c62e
Christian <christianlydemann@gmail.com>
2018-02-17 18:14:30 +0100

    removed unused

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,7 @@
-const path = require('path');
 const express = require('express');
 
 exports = module.exports = function(app) {
 
-    const staticFilesPath = path.resolve('./static');
-  
     app.get('/hello', (req, res) => res.send('Hello World!'))
 }
   
